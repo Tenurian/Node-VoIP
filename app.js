037a41b5c89f057dae88b033db2de8d9b86fa6fe
Thomas Feuerborn <tfeuerbo@gmail.com>
2017-05-11 11:13:29 -0600

    bort

---
+++ app.js
@@ -0,0 +1,25 @@
+var PORT = 2319;
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+app.get('/', function(req, res) { res.send('Hello world!'); });
+
+var server = app.listen(PORT);
+
+var options = {
+    debug: true
+  };
+
+//app.use('/api', ExpressPeerServer(server, options));
+// // OR
+
+var server = require('http').createServer(app);
+
+app.use('/public', ExpressPeerServer(server, options));
+
+server.on('connection', function(id) {
+    console.log(id);
+  });
+
+server.listen(PORT);
